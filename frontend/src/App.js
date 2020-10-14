dc9d575b4e9d3661fdac9c9a3bd3f8cae807ae95
orvengonzales <orven.gozales@gmail.com>
2020-10-14 16:47:33 +0800

    React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome</h1>
+    </>
+  );
+}
+
+export default App;
