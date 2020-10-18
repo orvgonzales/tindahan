15b6ee5465d2e7841dcd789274dbf046ae2e96ef
orvengonzales <orven.gozales@gmail.com>
2020-10-18 12:47:13 +0800

    Added Hooks

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
+
 
 const ProductScreen = ({match}) => {
-    const product = products.find((p) => p._id === match.params.id)
+    const[product, setProduct] = useState({});
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+          const { data } = await axios.get(`/api/products/${match.params.id}`)
+    
+          setProduct(data)
+        }
+    
+        fetchProduct()
+      }, [])
     
     return (
         <>
@@ -25,10 +36,10 @@ const ProductScreen = ({match}) => {
                         <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
                     </ListGroup.Item>
                     <ListGroup.Item>
-                        Price: ${product.price}
+                        Price: ₱{product.price}
                     </ListGroup.Item>
                     <ListGroup.Item>
-                        Description: ${product.description}
+                        Description: {product.description}
                     </ListGroup.Item>
                 </ListGroup>
             </Col>
@@ -68,4 +79,4 @@ const ProductScreen = ({match}) => {
     )
 }
 
-export default ProductScreen
+export default ProductScreen;
