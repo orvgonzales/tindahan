e460144bb64690331908e6e9d3c0f2811b73abd4
orvengonzales <orven.gozales@gmail.com>
2020-10-22 13:15:20 +0800

    Added details reducer for store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,21 +1,24 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk' 
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer } from './reducers/productReducers'
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
 
 const reducer = combineReducers({
-    productList: productListReducer,
-})
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+});
 
-const initialState = {}
+const initialState = {};
 
-const middleware = [thunk]
+const middleware = [thunk];
 
 const store = createStore(
-    reducer, 
-    initialState, 
-    composeWithDevTools(applyMiddleware(...middleware))
-    )
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
-export default store
-
+export default store;
