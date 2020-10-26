993c7c74bb575662c6afd6fc31c7de9d59ae1738
orvengonzales <orven.gozales@gmail.com>
2020-10-26 18:51:36 +0800

    Changed the Home text to Cart

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,30 +1,30 @@
 import React from "react";
-import { LinkContainer } from 'react-router-bootstrap';
+import { LinkContainer } from "react-router-bootstrap";
 import { Navbar, Nav, Container } from "react-bootstrap";
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-          <Navbar.Brand>Tindahan</Navbar.Brand>
+            <Navbar.Brand>Shop</Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-            <LinkContainer to='/cart'>
-              <Nav.Link>
-                <i className="fas fa-shopping-cart"></i>Home
-              </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to='/login'>
-              <Nav.Link>
-                <i className="fas fa-user"></i>Sign In
-              </Nav.Link>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i>Sign In
+                </Nav.Link>
               </LinkContainer>
             </Nav>
-          </Navbar.Collapse>   
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
