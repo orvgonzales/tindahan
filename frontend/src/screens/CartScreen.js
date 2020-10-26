fca0c1285f6984d159a3a8def1ceb20f6636ec14
orvengonzales <orven.gozales@gmail.com>
2020-10-26 18:56:48 +0800

    Cart Features Done

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -9,10 +9,9 @@ import {
   Form,
   Button,
   Card,
-  ListGroupItem,
 } from "react-bootstrap";
 import Message from "../components/Message";
-import { addToCart } from "../actions/cartActions";
+import { addToCart, removeFromCart } from "../actions/cartActions";
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id;
@@ -31,7 +30,7 @@ const CartScreen = ({ match, location, history }) => {
   }, [dispatch, productId, qty]);
 
   const removeFromCartHandler = (id) => {
-    console.log("remove");
+    dispatch(removeFromCart(id));
   };
 
   const checkoutHandler = () => {
