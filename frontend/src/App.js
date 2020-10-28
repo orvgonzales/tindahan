83ed42f43d67a83cdeea1bd79d8ba8d279908e5f
orvengonzales <orven.gozales@gmail.com>
2020-10-28 17:48:43 +0800

    User registration setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import CartScreen from "./screens/CartScreen";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
+          <Route path='/register' component={RegisterScreen} />
           <Route path='/login' component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen} exact />
           <Route path='/cart/:id?' component={CartScreen} exact />
