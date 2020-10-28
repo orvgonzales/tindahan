d9aa96224966dbeb145e66ecfa0cebc89d0a7a06
orvengonzales <orven.gozales@gmail.com>
2020-10-28 10:27:59 +0800

    Setup for the user authentication

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,36 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import { notFound, errorHandler} from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js'
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
-dotenv.config() //dotenv configuration 
+dotenv.config(); //dotenv configuration
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send("API is running") //Route Request for the backend 
-})
+app.use(express.json());
 
-app.use('/api/products', productRoutes)
+app.get("/", (req, res) => {
+  res.send("API is running"); //Route Request for the backend
+});
 
-app.use(notFound)
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
-app.use(errorHandler)
+app.use(notFound);
 
-const PORT = process.env.PORT || 5000
+app.use(errorHandler);
 
-app.listen(PORT, console.log(`Server running on ${process.env.NODE_ENV} mode port ${PORT}`.yellow.bold))
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running on ${process.env.NODE_ENV} mode port ${PORT}`.yellow.bold
+  )
+);
