89adaef98c70d729e5091f4b9d5c318403be8108
orvengonzales <orven.gozales@gmail.com>
2020-10-28 14:17:35 +0800

    User Login setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
   return (
@@ -13,9 +14,10 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/' component={HomeScreen} exact />
+          <Route path='/login' component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen} exact />
           <Route path='/cart/:id?' component={CartScreen} exact />
+          <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
