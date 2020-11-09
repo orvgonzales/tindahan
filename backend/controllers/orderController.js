dcf4eb6b58951d10e678f79e3017fe029521f23f
orvengonzales <orven.gozales@gmail.com>
2020-11-10 00:33:58 +0800

    Get order endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -86,4 +86,18 @@ const getMyOrders = asyncHandler(async (req, res) => {
   res.json(orders);
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
+// @desc    Get logged in user orders
+// @route   GET /api/orders/myorders
+// @access  Private
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate("user", "id name");
+  res.json(orders);
+});
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
+  getOrders,
+};
